3b861ed57fe06e300fa5a33e36d7062c9834b16d
Frohwalt Egerer <mail@frohwalt.de>
2013-05-15 16:26:36 +0200

    Reindent the source (after settings up emacs for clojure)

--- src/kata/core.clj
+++ src/kata/core.clj
@@ -3,28 +3,21 @@
 (defn third [s] (nth s 2)) 
 
 (defn remaining-rolls [rolls]
-	(cond
-	   (= 10 (first rolls))		(rest rolls)
-	   :else 					(nthrest rolls 2)
-	)
-)	
+  (cond
+   (= 10 (first rolls))  (rest rolls)
+   :else                 (nthrest rolls 2)))
 
 (defn frame-score [rolls]
-	(def framesum (+ (first rolls) (second rolls)))
-	(cond
-	   (= 10 (first rolls))		(+ 10 (second rolls) (third rolls))		
-	   (= 10 framesum) 			(+ framesum (third rolls))
-	   :else framesum
-	)
-)
+  (def framesum (+ (first rolls) (second rolls)))
+  (cond
+   (= 10 (first rolls))		(+ 10 (second rolls) (third rolls))		
+   (= 10 framesum) 			(+ framesum (third rolls))
+   :else framesum))
 
 (defn score-inner [rolls remaining-frames] 
-	(cond
-		(zero? remaining-frames) 0
-		:else	(+ (frame-score rolls)			   
-				   (score-inner (remaining-rolls rolls) (dec remaining-frames)))
-	)
-)
+  (cond
+   (zero? remaining-frames) 0
+   :else (+ (frame-score rolls)			   
+            (score-inner (remaining-rolls rolls) (dec remaining-frames)))))
 
-(defn score [rolls] 
-	(score-inner rolls 10))
+(defn score [rolls] (score-inner rolls 10))
