48186221cefe1ca42231f84fe7aa33438bda115d
Frohwalt Egerer <mail@frohwalt.de>
2013-05-16 23:03:35 +0200

    Add some comments

--- src/kata/core.clj
+++ src/kata/core.clj
@@ -4,11 +4,23 @@
 (defn spare?  [rolls]  (= 10 (+ (first rolls)) (second rolls)))
 (defn bonus?  [rolls]  (or (strike? rolls) (spare? rolls)))
 
+; A frame (other than the last one) can consist of 1 or 2 rolls. One
+; roll if the first one is a strike, 2 rolls with a spare or with
+; no bonus. So for rest-rolls we remove the correct number of rolls
+; from the front of the rolls sequence.
 (defn rest-rolls [rolls]
   (if (strike? rolls)
-    (rest rolls)
-    (nthrest rolls 2))
+    (rest rolls)              
+    (nthrest rolls 2))  
 
+; Depending on whether we need some bonus we calculate a frame score
+; by adding 2 rolls (normal) or 3 rolls (strike and spare).
+;
+; Interstingly both strike and spare are caluclated by summing
+; up three rolls: If we have a strike the current frame has only
+; one roll and we use two bonus rolls (total: 3), if we have a
+; spare the current frame has two rolls and we use one bonus roll.
+; (total: 3)
 (defn frame-score [rolls]
   (reduce + (take  (if (bonus? rolls)) 3 2)) rolls)))
 
